d0c0f3c14fd534430cef4fedc0978d944973316b
endsinger <102259704+endsinger@users.noreply.github.com>
2022-06-22 14:20:29 +0400

    Add files via upload

---
+++ ArabicToRomanNumbers2/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ArabicToRomanNumbers2
+{
+    class Class1
+    {
+        private string romanNum;
+
+        public string IntToRoman(int input)
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>
+            {
+                        { 1000, "M" },
+                        { 900, "CM"},
+                        { 500, "D" },
+                        { 400, "CD" },
+                        { 100, "C"},
+                        { 90, "XC" },
+                        { 50, "L" },
+                        { 40, "XL" },
+                        { 10, "X"},
+                        { 9, "IX"},
+                        { 5, "V" },
+                        { 4,"IV"},
+                        { 1, "I"},
+            };
+            
+            foreach (int i in dict.Keys)
+            {
+                while (input >= i)
+                {
+                    this.romanNum += dict[i];
+                    input -= i;
+                }
+            }
+            this.Print();
+            return this.romanNum;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Римское число: " + this.romanNum);
+        }
+    }
+}
---
+++ ArabicToRomanNumbers2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArabicToRomanNumbers2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                Class1 changeNumber = new Class1();
+                changeNumber.IntToRoman(input);
+            }
+            catch
+            {
+                Console.WriteLine("Введены неверные символы");
+            }
+            Console.ReadKey();
+        }
+    }
+}
